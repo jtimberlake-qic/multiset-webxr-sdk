bdd643e7ce855ac4587b3d1f1175e7dbb3bb6cb8
jtcreative <jtimberlake@quantumimprovements.net>
2025-10-23 15:04:58 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,12 @@
 import type { MapType } from "./interfaces";
 
 // Enter your credentials here
-export const CLIENT_ID = "YOUR_CLIENT_ID";
-export const CLIENT_SECRET = "YOUR_CLIENT_SECRET";
+//TestApp
+export const CLIENT_ID = "4f73bb67-c153-4e41-84d0-80e0b65b2da2";
+export const CLIENT_SECRET = "b02a35f595f717d6f0517ff9140d146b70a89a4aafa7073d3010a1a6a52eb393";
 export const CURRENT_MAP_TYPE: MapType = "map";
-export const CODE = "MapCode/MapsetCode";
+export const CODE = "MAP_UFD36OHV5XUD"; //"MapCode/MapsetCode";
+
 
 // API endpoints (do not modify)
 export const SDK_AUTH_URL = "https://api.multiset.ai/v1/m2m/token";
